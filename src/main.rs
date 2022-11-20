3cf044f778f0b749f3c5bbe544365deade47d6cb
benStre <strehle.benedikt@gmail.com>
2022-11-20 05:51:42 +0100

    working logger

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,24 @@
-use datex_core::compiler;
+use datex_core::{compiler, utils::logger::{Logger, LoggerContext}};
 
 mod command_line_args;
 use command_line_args::get_args;
+use lazy_static::lazy_static;
+
+lazy_static! {
+    static ref CTX:LoggerContext = LoggerContext {
+        log_redirect: None
+    };
+}
 
 
 
 
 fn main() {
-    let args = get_args();
-    println!("Hello DATEX!");
+    let logger = Logger::new_for_development(&CTX, "DATEX");
+    logger.success("initialized");
 
+    let args = get_args();
+    
     if args.file.is_some() {
         println!("File: {}", args.file.unwrap())
     }
